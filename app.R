d2f10be0ec2ae1b450125bcf608be2ea8a970815
Sebastian <sebastian@Sebastians-MacBook-Pro.local>
2018-09-05 13:55:03 -0500

    Add table with home keys on sidebar

--- app.R
+++ app.R
@@ -2,48 +2,40 @@ library(shiny)
 library(visNetwork)
 library(sqldf)
 library(readr)
+library(DT)
 
 ui <- fluidPage(
   titlePanel("Calidad de vida de una madre soltera"),
-  fluidPage( tabsetPanel(
-    tabPanel("Estructura familiar", 
-             sidebarLayout(sidebarPanel = verticalLayout(textInput("families", "Familia:", "0000"),
-                                                         uiOutput("campSelector"),
-                                                         textOutput("result")), 
-                           mainPanel = visNetworkOutput("network"))),
+  tabsetPanel(
+  tabPanel("Estructura del hogar", 
+           sidebarLayout(sidebarPanel(verticalLayout(textInput("families", "Familia:", "0000"),
+                                                     textOutput("result"),
+                                                     DT::dataTableOutput("table")),                                              renderTable('table')), 
+                           mainPanel(visNetworkOutput("network")))),
     tabPanel("Regresión lineal", "contents"),
-    tabPanel("Formulario", "contents")))
+    tabPanel("Formulario", "contents"))
 )
 
 server <- function(input, output) {
   #Load DB
   DataBase <- read_csv("Caracteristicas y composicion del hogar.csv")
   
-  # Select input
-  queryFamilies=sqldf("SELECT LLAVEHOG FROM DataBase group by LLAVEHOG")
-  IdsFamilies <- c("Nadie")
-  for (row in 1:nrow(queryFamilies)) {
-    IdsFamilies <- c(IdsFamilies, queryFamilies[row, 'LLAVEHOG'])
-  }
-  output$campSelector <- renderUI({
-    selectInput("familiesSelector", "Familias", IdsFamilies) 
-  })
-  
   #Evento de cuando cambia el campo de texto
-  #observeEvent(input$familiesSelector, { # Si se va a usar el selector quitar este comentario
-  # familyQuery$data <- input$familiesSelector # Si se va a usar el selector quitar este comentario
   observeEvent(input$families, { # Si se va a usar el texto quitar este comentario
     familyQuery$data <- input$families # Si se va a usar el text quitar este comentario
     output$result <- renderText({""})
   })
   familyQuery <- reactiveValues(data = NULL)
   
+  queryFamilies=sqldf("SELECT LLAVEHOG FROM DataBase group by LLAVEHOG")
+  output$table <- DT::renderDataTable(queryFamilies)
+  
   output$network <- renderVisNetwork({
     query=paste("SELECT * FROM DataBase WHERE LLAVEHOG = '",familyQuery$data,sep="")
     query=paste(query,"\' ORDER BY ORDEN",sep="")
     #Do query
-    queryFamilies = sqldf(query)
-    if(nrow(queryFamilies) == 0){
+    queryFamily = sqldf(query)
+    if(nrow(queryFamily) == 0){
       output$result <- renderText({ 
         paste("No se encontraron resultados con el número de familia ", familyQuery$data) 
       })
@@ -53,13 +45,13 @@ server <- function(input, output) {
       edgesTo = c()
       edgesFrom = c()
       label = c()
-      for (row in 1:nrow(queryFamilies)) {
+      for (row in 1:nrow(queryFamily)) {
         label = c(label, row)
-        sex <- queryFamilies[row, 'P6020']
-        relationship <- queryFamilies[row, 'P6051']
-        father <- queryFamilies[row, 'P6081']
-        mother <- queryFamilies[row, 'P6083']
-        spouse <- queryFamilies[row, 'P6071']
+        sex <- queryFamily[row, 'P6020']
+        relationship <- queryFamily[row, 'P6051']
+        father <- queryFamily[row, 'P6081']
+        mother <- queryFamily[row, 'P6083']
+        spouse <- queryFamily[row, 'P6071']
         #Color node
         if(sex == 1){
           Sex = c(Sex, 'skyblue')
@@ -82,26 +74,25 @@ server <- function(input, output) {
           }
         }
         
-        if(father == 1 && queryFamilies[row, 'P6081S1'] != 1){
+        if(father == 1 && queryFamily[row, 'P6081S1'] != 1){
           edgesFrom <- c(edgesFrom, row)
-          edgesTo <- c(edgesTo, queryFamilies[row, 'P6081S1'])
+          edgesTo <- c(edgesTo, queryFamily[row, 'P6081S1'])
         }
         
-        if(mother == 1 && queryFamilies[row, 'P6083S1'] != 1){
+        if(mother == 1 && queryFamily[row, 'P6083S1'] != 1){
           edgesFrom <- c(edgesFrom, row)
-          edgesTo <- c(edgesTo, queryFamilies[row, 'P6083S1'])
+          edgesTo <- c(edgesTo, queryFamily[row, 'P6083S1'])
         }
         
-        if(spouse == 1 && queryFamilies[row, 'P6071S1'] != 1 && queryFamilies[row, 'P6071S1'] > row){
+        if(spouse == 1 && queryFamily[row, 'P6071S1'] != 1 && queryFamily[row, 'P6071S1'] > row){
           edgesTo <- c(edgesTo, row)
-          edgesFrom <- c(edgesFrom, queryFamilies[row, 'P6071S1'])
-          edgesTo <- c(edgesTo, queryFamilies[row, 'P6071S1'])
+          edgesFrom <- c(edgesFrom, queryFamily[row, 'P6071S1'])
+          edgesTo <- c(edgesTo, queryFamily[row, 'P6071S1'])
           edgesFrom <- c(edgesFrom, row)
         }
       }
       
-      # minimal example
-      nodes <- data.frame(id = 1:nrow(queryFamilies), 
+      nodes <- data.frame(id = 1:nrow(queryFamily), 
                           color = Sex,
                           label = label)
       edges <- data.frame(from = edgesFrom, to = edgesTo)
